6fa8117ff65b6bdb044e96d35586beb26de41ebd
mabouelkhir <mohamedabouelkhir34@gmail.com>
2023-12-25 20:44:33 +0100

    commit

--- Client/src/main/java/com/example/client/Models/CarClient.java
+++ Client/src/main/java/com/example/client/Models/CarClient.java
@@ -4,8 +4,7 @@ package com.example.client.Models;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.service.annotation.GetExchange;
-import org.springframework.web.service.annotation.HttpExchange;
+
 
 import java.util.List;
 
